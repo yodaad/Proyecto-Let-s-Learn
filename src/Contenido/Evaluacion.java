b81ea18d1ee376408f21482ef2fccac27b66bacd
yodaad <yodaad@gmail.com>
2021-06-14 17:08:34 -0500

    Actualización clase Evaluación

--- src/Contenido/Evaluacion.java
+++ src/Contenido/Evaluacion.java
@@ -2,26 +2,21 @@ package Contenido;
 
 
 public class Evaluacion {
-	
-//	protected String respuestausuario;
-	protected boolean respuesta=true;
+
+	private boolean respuesta;
 	Paises pais=new Paises();
-	
+
 	public Evaluacion () {
-		
+
 	}
-	
+
 	public void setRespuesta (String respuestausuario) {
-		if (respuestausuario.equalsIgnoreCase(pais.getPais())) {
-			this.respuesta=true;
-		} else {
-			this.respuesta=false;
-		}
+		respuesta=respuestausuario.equalsIgnoreCase(pais.getPais());
 	}
-	
+
 	public boolean getRespuesta () {
 		return respuesta;
 	}
 
-	
+
 }
